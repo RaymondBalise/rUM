812f86c0820f28669c322e2e1a274367c1af3082
Kyle Grealis <kylegrealis@icloud.com>
2024-11-07 14:30:59 -0600

    experimental: source run me first?

--- R/make_project.R
+++ R/make_project.R
@@ -314,7 +314,9 @@ make_project <- function(
   } 
   
   ### EXPERIMENTAL ###
-  # source(paste0(path, "/RUN_ME_FIRST.R"))
+  # if (vignette == TRUE) {
+  #   source(paste0(path, "/RUN_ME_FIRST.R"))
+  # }
   
 }
 
