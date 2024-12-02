08042dbb9f05ebc4611d194c229c26b7a5ac53dd
Kyle Grealis <kylegrealis@icloud.com>
2024-12-02 15:23:51 -0600

    edit: improved logic for handling if existing SCSS file exists

--- R/write_scss.R
+++ R/write_scss.R
@@ -22,79 +22,84 @@
 #' }
 
 write_scss <- function(name = 'custom', path = getwd()) {
-
+  
   # Check if directory exists
   if (!dir.exists(path)) {
-    # Exit if directory does not exist
-    stop('Directory does not exist') 
+    stop('Directory does not exist')
     return(NULL)
   }
-
+  
   # Normalize the path for consistency
   path <- normalizePath(path, mustWork = TRUE)
-
-  # Warn user if a .scss file is found in project
+  
+  # Define the target file path
+  the_scss_file <- file.path(path, paste0(name, '.scss'))
+  
+  # Check for existing .scss files
   listed_files <- list.files(
-    path = path, 
-    pattern = '\\.scss$', 
-    full.names = TRUE, 
+    path = path,
+    pattern = '\\.scss$',
+    full.names = TRUE,
     recursive = FALSE
   )
   
+  # Determine if we should proceed with file creation
+  proceed <- TRUE
+  
   if (length(listed_files) > 0) {
     ui_info('**CAUTION!!**')
+    
     # Check if file with same name exists
     if (any(basename(listed_files) == paste0(name, '.scss'))) {
       ui_info(paste0('A file named "', name, '.scss" already exists!'))
-      abort <- ui_nope('Do you want to overwrite this specific file?')
+      proceed <- ui_yeah('Do you want to overwrite this specific file?')
     } else {
       ui_info(paste0('Other .scss files exist but none named "', name, '.scss"'))
-      abort <- ui_nope('Proceed with creating another SCSS file?')
+      proceed <- ui_yeah('Would you like to create another SCSS file?')
     }
   }
-
-  # Define SCSS content
-  content <- glue::glue(
-'/*-- scss:defaults --*/
-// Colors
-// $primary: #2c365e;  
-// $body-bg: #fefefe;
-// $link-color: $primary;
-
-// Fonts
-// $font-family-sans-serif: "Open Sans", sans-serif;
-// $font-family-monospace: "Source Code Pro", monospace;
-
-/*-- scss:mixins --*/
-
-/*-- scss:rules --*/
-// Custom theme rules
-// .title-block {{
-//   margin-bottom: 2rem;
-//   border-bottom: 3px solid $primary;
-// }}
-
-// code {{
-//   color: darken($primary, 10%);
-//   padding: 0.2em 0.4em;
-//   border-radius: 3px;
-// }}'
-  )
-
-  # Write SCSS file
-  the_scss_file <- file.path(path, paste0(name, '.scss'))
-  writeLines(content, the_scss_file)
-  ui_done(paste0('Created ', name, '.scss'))
-
-  # Display reference links
-  links <- glue::glue(
-    'For more SCSS styling options, visit:
-    - https://quarto.org/docs/output-formats/html-themes.html#customizing-themes
-    - https://quarto.org/docs/output-formats/html-themes-more.html
-    - https://github.com/twbs/bootstrap/blob/main/scss/_variables.scss'
-  )
-  ui_info(links)
-
+  
+  # Only proceed if user confirmed
+  if (proceed) {
+    # Define SCSS content
+    content <- glue::glue(
+      '/*-- scss:defaults --*/
+      // Colors
+      // $primary: #2c365e;  
+      // $body-bg: #fefefe;
+      // $link-color: $primary;
+      // Fonts
+      // $font-family-sans-serif: "Open Sans", sans-serif;
+      // $font-family-monospace: "Source Code Pro", monospace;
+      /*-- scss:mixins --*/
+      /*-- scss:rules --*/
+      // Custom theme rules
+      // .title-block {{
+      //   margin-bottom: 2rem;
+      //   border-bottom: 3px solid $primary;
+      // }}
+      // code {{
+      //   color: darken($primary, 10%);
+      //   padding: 0.2em 0.4em;
+      //   border-radius: 3px;
+      // }}'
+    )
+    
+    # Write SCSS file
+    writeLines(content, the_scss_file)
+    ui_done(paste0('Created ', name, '.scss'))
+    
+    # Display reference links
+    links <- glue::glue(
+      'For more SCSS styling options, visit:
+      - https://quarto.org/docs/output-formats/html-themes.html#customizing-themes
+      - https://quarto.org/docs/output-formats/html-themes-more.html
+      - https://github.com/twbs/bootstrap/blob/main/scss/_variables.scss'
+    )
+    ui_info(links)
+  } else {
+    ui_info("Operation cancelled - no file was created or modified.")
+  }
+  
   return(invisible(NULL))
-
- }
+}
