2c736632efee0448a788c826b8fe39900f348d29
Kyle Grealis <kylegrealis@icloud.com>
2024-12-03 08:10:34 -0600

    updated checking for existing files & overwriting

--- R/write_readme.R
+++ R/write_readme.R
@@ -39,17 +39,47 @@ write_readme <- function(path = getwd()) {
   if (readme_path == "") {
     stop("Could not find README template in package installation")
   }
+
+  # Handle dated_progress_notes.md creation/overwrite
+  progress_notes_content <- paste0(
+    "# Add project updates here\n",
+    format(Sys.Date(), "%b %d, %Y"),
+    ": project started"
+  )
   
   # Handle README creation/overwrite
   if (file.exists(file.path(path, 'README.md'))) {
     ui_info('**CAUTION!!**')
     if (ui_yeah('README.md found in project level directory! Overwrite?')) {
+
+      # Yes, overwrite existing README
       invisible(file.copy(
         from = readme_path,
         to = file.path(path, "README.md"),
         overwrite = TRUE
       ))
       ui_done("README.md has been overwritten with the template.")
+
+      # Now check if the progress notes exists & ask to overwrite that too
+      if (file.exists(file.path(path, 'dated_progress_notes.md'))) {
+        ui_info('**CAUTION!!**')
+        if (ui_yeah('dated_progress_notes.md found in project level directory! Overwrite?')) {
+          writeLines(
+            progress_notes_content,
+            con = file.path(path, "dated_progress_notes.md")
+          )
+          ui_done("dated_progress_notes.md has been overwritten with the template.")
+        } else {
+          ui_info("Keeping existing dated_progress_notes.md")
+        }
+      } else {
+        writeLines(
+          progress_notes_content,
+          con = file.path(path, "dated_progress_notes.md")
+        )
+        ui_done("A dated_progress_notes.md template has been created.")
+      }
+
     } else {
       ui_info("Keeping existing README.md")
     }
@@ -59,32 +89,12 @@ write_readme <- function(path = getwd()) {
       to = file.path(path, "README.md")
     ))
     ui_done("A README.md template has been created.")
-  }
-  
-  # Handle dated_progress_notes.md creation/overwrite
-  progress_notes_content <- paste0(
-    "# Add project updates here\n",
-    format(Sys.Date(), "%b %d, %Y"),
-    ": project started"
-  )
-  
-  if (file.exists(file.path(path, 'dated_progress_notes.md'))) {
-    ui_info('**CAUTION!!**')
-    if (ui_yeah('dated_progress_notes.md found in project level directory! Overwrite?')) {
-      writeLines(
-        progress_notes_content,
-        con = file.path(path, "dated_progress_notes.md")
-      )
-      ui_done("dated_progress_notes.md has been overwritten with the template.")
-    } else {
-      ui_info("Keeping existing dated_progress_notes.md")
-    }
-  } else {
+
     writeLines(
       progress_notes_content,
       con = file.path(path, "dated_progress_notes.md")
     )
-    ui_done("A dated_progress_notes.md template has been created.")
+    ui_done("dated_progress_notes.md has been created.")
   }
   
   return(invisible(NULL))
